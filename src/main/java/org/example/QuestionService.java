67a7da154e56b946723319f486b78a981377e35c
igr1 <dfrgth>
2022-10-22 08:40:35 +1000

    cursovaya 2 start

---
+++ src/main/java/org/example/QuestionService.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Collection;
+
+public interface QuestionService {
+
+    Question add(String question, String answer);
+
+    Question add(Question question);
+
+    Question remove(Question question);
+
+    Collection<Question> getAll();
+
+    Question getRandomQuestion();
+}
